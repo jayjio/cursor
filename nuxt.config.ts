e5664ff6d95b5bae51c75b29657855827950b89f
jayjio <hellojeremygio@gmail.com>
2025-11-09 22:23:15 -0700

    Add Nitro routeRules for CORS configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,5 +16,15 @@ export default defineNuxtConfig({
   nitro: {
     // Nitro automatically serves files from .output/public/
     // Widget files will be available at /widget/* after build
+    routeRules: {
+      '/api/chat': {
+        cors: true,
+        headers: {
+          'Access-Control-Allow-Methods': 'POST, OPTIONS, GET',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+          'Access-Control-Max-Age': '86400'
+        }
+      }
+    }
   }
 })
